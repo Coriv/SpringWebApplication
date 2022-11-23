c5695b1416d4b264ef6d964dad3bbcebf9435280
Sebastian Boron <sebastian.boron8@gmail.com>
2022-11-23 19:11:59 +0100

    23.4 Scheduled annotation added

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.respository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class EmailScheduler {
+    private final SimpleEmailService emailService;
+    private final AdminConfig adminConfig;
+    private final TaskRepository taskRepository;
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        emailService.send(Mail.builder()
+                .mailTo(adminConfig.getAdminMail())
+                .subject(SUBJECT)
+                .message(messageBuilder(size))
+                .build());
+    }
+
+    private String messageBuilder(long size) {
+        String result = "Currently in database you got: " + size;
+        if (size == 1) {
+            return result + " task";
+        }
+        return result + " tasks";
+    }
+}
