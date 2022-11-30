0909c0a0a27e151f1acc86720d5f5e2a6bfd0447
Sebastian Boron <sebastian.boron8@gmail.com>
2022-11-30 13:55:38 +0100

    Validation tests added

--- src/test/java/com/crud/tasks/trello/validator/TrelloValidatorTestSuite.java
+++ src/test/java/com/crud/tasks/trello/validator/TrelloValidatorTestSuite.java
@@ -1,4 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.crud.tasks.trello.validator;
+
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloCard;
+import com.crud.tasks.domain.TrelloList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
 class TrelloValidatorTestSuite {
-  
+
+    @Autowired
+    private TrelloValidator trelloValidator;
+
+    @Test
+    void shouldLogInfoAboutTest() {
+        TrelloCard trelloCard = new TrelloCard("12", "test", "testDesc", "top");
+        trelloValidator.validateCard(trelloCard);
+    }
+
+    @Test
+    void shouldLogInfoAboutTask() {
+        TrelloCard trelloCard = new TrelloCard("12", "name", "testDesc", "top");
+        trelloValidator.validateCard(trelloCard);
+    }
+
+    @Test
+    void shouldFilterTestTrelloBoards() {
+        //Given
+        List<TrelloList> trelloList = List.of(new TrelloList("ListId", "Test", false));
+        TrelloBoard trelloTestBoard = new TrelloBoard("123", "test", trelloList);
+        TrelloBoard trelloBoard = new TrelloBoard("324", "No test", trelloList);
+
+        List<TrelloBoard> boardsList = Arrays.asList(trelloBoard, trelloTestBoard);
+        //When
+        List<TrelloBoard> resultList = trelloValidator.validateTrelloBoards(boardsList);
+        //Then
+        Assertions.assertEquals(resultList.size(), 1);
+    }
 }
