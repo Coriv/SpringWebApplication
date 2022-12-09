bae64f0eb5f15b68e13bb84824ef926e846eef4c
Sebastian Boron <sebastian.boron8@gmail.com>
2022-12-09 21:39:28 +0100

    generation

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -13,8 +13,8 @@ import javax.persistence.*;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     @Column(name = "name")
     private String title;
